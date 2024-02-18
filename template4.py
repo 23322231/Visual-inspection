2437d54a1915452259e9a16233616260993fe2b2
minchi39 <126585515+minchi39@users.noreply.github.com>
2024-02-18 21:44:48 +0800

    未成功

--- template4.py
+++ template4.py
@@ -12,9 +12,7 @@ def index():
 @app.route('/receive_coordinates', methods=['POST'])
 def receive_coordinates():
     data = request.get_json()
-    # 在这里处理接收到的坐标数据
     print("Received coordinates:", data)
-    # 在这里执行你的后续处理逻辑，比如保存到数据库、进一步处理等
     return jsonify({'message': 'Coordinates received successfully'})
 
 if __name__ == '__main__':
