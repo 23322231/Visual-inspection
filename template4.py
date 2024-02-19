098646c919bb3a34f57157a3dd3ae9eadd843fcf
minchi39 <126585515+minchi39@users.noreply.github.com>
2024-02-19 20:36:07 +0800

    成功了

--- template4.py
+++ template4.py
@@ -1,6 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 10:56:02 2024
 
+@author: admin
+"""
+
+"""
+2/9 chatgpt
+"""
 from flask import Flask, request, jsonify
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
@@ -8,12 +18,14 @@ app = Flask(__name__)
 def index():
     return render_template('handwrite1.html')
 
-
+# 在 /receive_coordinates 路由上監聽來自客戶端的 POST 請求
 @app.route('/receive_coordinates', methods=['POST'])
 def receive_coordinates():
-    data = request.get_json()
+    data = request.get_json()   # 從 POST 請求中取得 JSON 格式的資料
     print("Received coordinates:", data)
+    # 返回一個 JSON 格式的回應，其中包含一個名為 message 的屬性，指示座標資料已成功接收
     return jsonify({'message': 'Coordinates received successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
