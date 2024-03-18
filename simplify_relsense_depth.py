d5da7e8bdd3a46479e5cdb86afb60da1840c427e
April-ying <april910909@gmail.com>
2024-03-18 12:17:36 +0800

    update:simplify是try的簡化
    只留下需要顯示的影像，把深度影像拿掉了

--- simplify_relsense_depth.py
+++ simplify_relsense_depth.py
@@ -25,19 +25,16 @@ if not found_rgb:
     exit(0)
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-# if device_product_line == 'L500':
 config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-# else:
-    # config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
 
 # Start streaming
 pipeline.start(config)
 
 try:
+    
     while True:
 
-        # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames() #等待串流 pipeline 中的一組幀。這些幀通常包括深度影像、彩色影像等
         depth_frame = frames.get_depth_frame()#返回深度影像幀。
         color_frame = frames.get_color_frame()#返回彩色影像幀。
@@ -46,8 +43,9 @@ try:
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())#獲取深度影像的資料並轉換為 NumPy 陣列
-        color_image = np.asanyarray(color_frame.get_data())#獲取彩色影像的資料並轉換為 NumPy 陣列
-
+        color_image = np.asanyarray(color_frame.get_data())#獲取彩色影像的資料並轉換為 NumPy 陣列 type:numpy.ndarray
+         
+        
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         #將深度影像轉換為彩色深度圖 (depth colormap)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
@@ -56,7 +54,6 @@ try:
         depth_colormap_dim = depth_colormap.shape
         color_colormap_dim = color_image.shape
 
-        # If depth and color resolutions are different, resize color image to match depth image for display
         #確保深度彩色圖 (depth_colormap) 和彩色影像 (color_image) 的尺寸一致
         if depth_colormap_dim != color_colormap_dim:
             resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)#interpolation參數指定了調整大小時的插值方法
@@ -69,11 +66,11 @@ try:
         center_pixel_y = depth_frame.height // 2
         depth_value_center = depth_frame.get_distance(center_pixel_x, center_pixel_y)
 
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.circle(images, (center_pixel_x, center_pixel_y), 5, (0, 255, 0), -1)
-        cv2.imshow('RealSense', images)
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)#創建一個名為 "RealSense" 的視窗，並設置視窗的顯示方式為自動調整大小 
+        cv2.circle(color_image, (color_frame.width // 2, color_frame.height // 2), 5, (0, 255, 0), -1)
+        cv2.imshow('RealSense', color_image)
          
-        print("Depth value at center pixel:", depth_value_center ,"m")
+        print("Depth value at center pixel:", depth_value_center*100 ,"cm")
         cv2.waitKey(1)
 
 finally:
