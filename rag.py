dd622e5d40b27b0f757083679009764677697d2c
April <april910909@gmail.com>
2024-04-12 20:14:28 +0800

    rag

---
+++ rag.py
@@ -0,0 +1,6 @@
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import LlamaCpp
+from langchain.chains import RetrievalQA
