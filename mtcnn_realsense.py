fef0fd2b16c30269cd3095f4cda7bfe174dc1c0c
April <april910909@gmail.com>
2024-04-22 01:21:55 +0800

    update:量測眼睛中心點的部分算是完成了

--- mtcnn_realsense.py
+++ mtcnn_realsense.py
@@ -45,19 +45,26 @@ if not found_rgb:
     exit(0)
 
 rs_config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)#啟用深度流，設置解析度為 640x480，像素格式為 z16（16 位深度值），幀率為 30 幀/秒
-rs_config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)#啟用彩色流，設置解析度為 960x540，像素格式為 bgr8（8 位 RGB 彩色），幀率為 30 幀/秒
+rs_config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)#啟用彩色流，設置解析度為 960x540，像素格式為 bgr8（8 位 RGB 彩色），幀率為 30 幀/秒
 
 
 # Start streaming
 pipeline.start(rs_config)
 
+align_to=rs.stream.color
+align=rs.align(align_to)
 try:
     while True:
         eye_center_x=0
         eye_center_y=0
+        depth_value=0
 
         # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames() #等待串流 pipeline 中的一組幀。這些幀通常包括深度影像、彩色影像等
+        frame = pipeline.wait_for_frames() #等待串流 pipeline 中的一組幀。這些幀通常包括深度影像、彩色影像等
+        frames=align.process(frame)
+        # print(1)
+        # print(type(frames))
+        # print((frames.size()))
         depth_frame = frames.get_depth_frame()#返回深度影像幀。
         color_frame = frames.get_color_frame()#返回彩色影像幀。
         if not depth_frame or not color_frame:#檢查是否成功獲取了深度影像和彩色影像幀
@@ -66,21 +73,23 @@ try:
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())#獲取深度影像的資料並轉換為 NumPy 陣列
         color_image = np.asanyarray(color_frame.get_data())#獲取彩色影像的資料並轉換為 NumPy 陣列
-        img=np.asanyarray(color_frame.get_data())
+        
         
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         #將深度影像轉換為彩色深度圖 (depth colormap)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
         #彩色圖和彩色影像的尺寸資訊
-        depth_colormap_dim = depth_colormap.shape
+        depth_colormap_dim = depth_image.shape
         color_colormap_dim = color_image.shape
+        # print(depth_colormap_dim)
+        # print(color_colormap_dim)
         
         # #人臉檢測
         t_1 = time.time()
         # img：待檢測的彩色影像。minsize：人臉的最小尺寸。pnet、rnet、onet：MTCNN 模型的三個子網絡。threshold：三個步驟的閾值。factor：縮放因子。
         # bounding_boxes：檢測到的人臉的邊界框座標，是一個 Numpy 陣列。points：檢測到的人臉的特徵點座標，也是一個 Numpy 陣列。
-        bounding_boxes, points = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        bounding_boxes, points = detect_face.detect_face(color_image, minsize, pnet, rnet, onet, threshold, factor)
         d_t = time.time() - t_1
 
         #邊界框處理
@@ -92,7 +101,7 @@ try:
 
             det = bounding_boxes[:, 0:4]#(左上角 x, 左上角 y, 右下角 x, 右下角 y)
             det_arr = []
-            img_size = np.asarray(img.shape)[0:2]#得到圖像的高度和寬度
+            img_size = np.asarray(color_image.shape)[0:2]#得到圖像的高度和寬度
             detect_multiple_faces=False#處理多個檢測到的人臉或僅處理其中的一個
             
             if nrof_faces > 1:
@@ -115,17 +124,34 @@ try:
             det_arr = det_arr.astype(np.int16)
 
             for i, det in enumerate(det_arr):#遍歷 det_arr 中的每個邊界框
-                cv2.rectangle(img, (det[0],det[1]), (det[2],det[3]), color, 2)#在原始影像上繪製一個矩形
+                cv2.rectangle(color_image, (det[0],det[1]), (det[2],det[3]), color, 2)#在原始影像上繪製一個矩形
 
                 #在人臉上繪製 5 個特徵點
                 facial_points = points[i]
                 for j in range(0,5,1):
                     #cv2.circle(影像, 圓心座標, 半徑, 顏色, 線條寬度)
-                    cv2.circle(img, (facial_points[j], facial_points[j + 5]), 2, (0, 0, 255), -1, 1)
+                    cv2.circle(color_image, (facial_points[j], facial_points[j + 5]), 2, (0, 0, 255), -1, 1)
 
+                # 取出左右眼的位置座標
+                left_eye_x, left_eye_y = facial_points[0], facial_points[1]
+                right_eye_x, right_eye_y = facial_points[2], facial_points[3]
+                print("Left eye coordinates:", (left_eye_x, left_eye_y))
+                print("Right eye coordinates:", (right_eye_x, right_eye_y))
+                print("eye_center_x",(right_eye_x + left_eye_x)//2)
+                print("eye_center_y",(right_eye_y + left_eye_y)//2)
+                eye_center_x=(right_eye_x + left_eye_x)//2
+                eye_center_y=(right_eye_y + left_eye_y)//2
+
+                if 0 <= eye_center_x < depth_image.shape[1] and 0 <= eye_center_y < depth_image.shape[0]:
+                    # 從深度影像中獲取眼睛中心點的深度值
+                    depth_value = depth_frame.get_distance(eye_center_y, eye_center_x)
+                    # 眼睛中心點與相機的距離
+                    print("Distance from camera to eye center (in meters):", depth_value)
+                else:
+                    print("Eye center point is out of bounds of depth image.")
         #未檢測到人臉
         else:
-            cv2.putText(img, no_face_str, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+            cv2.putText(color_image, no_face_str, (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
         #計算並顯示每秒幀數（FPS）
         if frame_count == 0:
@@ -136,14 +162,10 @@ try:
             frame_count = 0
 
         # cv2.putText(影像, 文字, 座標, 字型, 大小, 顏色, 線條寬度, 線條種類)
-        cv2.putText(img, FPS, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+        cv2.putText(color_image, FPS, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
         
         #將深度影像轉換為彩色深度圖 (depth colormap)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-        #彩色圖和彩色影像的尺寸資訊
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
 
         # If depth and color resolutions are different, resize color image to match depth image for display
         #確保深度彩色圖 (depth_colormap) 和彩色影像 (color_image) 的尺寸一致
