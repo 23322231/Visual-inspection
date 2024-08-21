e8a22528d9cdb16c035b69cec1b53bbc9e3cefad
April <april910909@gmail.com>
2024-08-21 11:03:17 +0800

    update:目前應該都是ok的版本，太久沒上傳先存一次

--- app.py
+++ app.py
@@ -8,26 +8,41 @@ import random
 import uuid
 import os
 
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # 用於會話加密的密鑰
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://april0909:c7CslksYkeusqcvAkMecoFDQPIFuiPKp@dpg-cqcj0sg8fa8c73crb3u0-a.oregon-postgres.render.com/data_uire"
+#第一次的資料庫
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://april0909:c7CslksYkeusqcvAkMecoFDQPIFuiPKp@dpg-cqcj0sg8fa8c73crb3u0-a.oregon-postgres.render.com/data_uire"
+#第二次
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://april0909:TevhMabtcGLrRlyn1rqrnfVcI5sVIKsH@dpg-cr1ljs5umphs73afhad0-a.oregon-postgres.render.com/data_0ol7"
 
 db = SQLAlchemy(app)
-socketio = SocketIO(app)
+socketio = SocketIO(app , cors_allowed_origins="*") # cors_allowed_origins="*" 可以允許任何来源的跨域請求。
+
+current_image = None
 
 # 連線資料庫的table
+# 色盲點圖的題目圖片
 class pic(db.Model):
-    __tablename__='images'
+    __tablename__='color_blind_question_pic'
     id=db.Column(db.Integer,primary_key=True)
     image_data=db.Column(db.String(150))
 
     def __init__(self, image_data):
         self.image_data = image_data
-
+# 色盲點圖的答案圖片
 class ans(db.Model):
-    __tablename__='user_ans'
+    __tablename__='color_blind_ans_pic'
+    id = db.Column(db.Integer, primary_key=True)
+    image_data = db.Column(db.LargeBinary)
+
+    def __init__(self, image_data=None):
+        self.image_data = image_data
+
+class user_ans(db.Model):
+    __tablename__='color_blind_user_ans_pic'
     id = db.Column(db.Integer, primary_key=True)
     image_data = db.Column(db.LargeBinary)
 
@@ -51,7 +66,7 @@ def choose():
     return render_template('camera_try.html')
 
 @app.route('/myopia')
-def confirm():
+def myopia():
     return render_template('myopia.html')
 
 @app.route('/generic')
@@ -63,9 +78,39 @@ def generic():
         return render_template('handwrite.html', data=base64_data)
     else:
         return "No image found", 404
+    
+#點圖製作功能頁面
+@app.route('/ishihara-test')
+def elements():
+    return render_template('ishihara-test.html')
+
+@app.route('/comfirm_colordot')
+def comfirm_colordot():
+    return render_template('comfirm_colordot.html')
+    
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    data = request.json
+    user_id = request.args.get('session')
+    if 'image' not in data:
+        return jsonify({'error': 'No image data found'})
+    
+    image_data = data['image']
+    image_data = image_data.replace('data:image/png;base64,', '')
+    binary_image_data = base64.b64decode(image_data)
+
+    new_image = user_ans(image_data=binary_image_data,user_id=user_id)#
+
+    db.session.add(new_image)
+    db.session.commit()
+
+    image = Image.open(BytesIO(base64.b64decode(image_data)))
+    image.save('uploaded_image.png')
+    return jsonify({'message': 'Image uploaded successfully'})
 
 @app.route('/next-image')
 def next_image():
+    print("執行了")
     random_id = random.randint(1, 20)
     colorblind_test = db.session.query(pic).filter(pic.id == random_id).first()
     if colorblind_test:
@@ -74,85 +119,65 @@ def next_image():
         return jsonify({'nextImageUrl': next_image_url})
     else:
         return jsonify({'error': 'No image found'}), 404
-    
-@app.route('/ishihara-test')
-def elements():
-    return render_template('ishihara-test.html')
 
-@app.route('/comfirm_colordot')
-def comfirm_colordot():
-    return render_template('comfirm_colordot.html')
-
-# @app.route('/handwrite')
-# def handwrite():
-#     user_uuid = request.args.get('user')  # 獲取查詢參數中的 user UUID
-#     if user_uuid:
-#         # 可以在這裡進行一些檢查或其他操作，比如驗證 UUID 格式
-#         return render_template('handwrite.html', user_uuid=user_uuid)
-#     else:
-#         return "User UUID not provided", 400
+# -------------------以下還沒debug結束---------------------- #
+# 由handwrite.html發送'img-connect'加上下一張題目的圖片的Base64編碼資料 
+@socketio.on('img-connect')
+def handle_connect(data):
+    print("有喔喔 喔喔喔喔")
+    current_image=data.get('background-image')
+    if current_image:
+        print("發送!")
+        #發送圖片資料給電腦端
+        emit('update_image', {'image': current_image},broadcast=True)
     
+
+@socketio.on('confirmDrawing')
+def handle_confirm_drawing(data):
+    print("Received confirmDrawing event")  # 確認事件觸發
+    url_suffix = data.get('urlSuffix')
+    if url_suffix:
+        # print("=======")
+        print(f'Received urlSuffix: {url_suffix}')
+        emit('confirm', {'urlSuffix': url_suffix},broadcast=True)
+    else:
+        print('No URL suffix provided.')
+
 @app.route('/handwrite')
 def handwrite():
     user_uuid = request.args.get('session')  # 从查询参数中获取 session ID
     if user_uuid:
-        # 在这里可以做进一步的处理，比如验证 session ID
+        
         return render_template('handwrite.html', user_uuid=user_uuid)
     else:
         return "User UUID not provided", 400
 
-
-@app.route('/show')
-def show():
-    return render_template('show.html')
-
 @app.route('/color_blind_spot_map')
 def color_blind_spot_map():
-    return render_template('color_blind_spot_map.html')
+    user_uuid = request.args.get('session')  # 从查询参数中获取 session ID
+    if user_uuid:
+        
+        return render_template('/color_blind_spot_map.html', user_uuid=user_uuid)
+    else:
+        return "User UUID not provided", 400
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    data = request.json
-    if 'image' not in data:
-        return jsonify({'error': 'No image data found'})
-    
-    image_data = data['image']
-    image_data = image_data.replace('data:image/png;base64,', '')
-    binary_image_data = base64.b64decode(image_data)
-
-    new_image = ans(image_data=binary_image_data)
-    db.session.add(new_image)
-    db.session.commit()
-
-    image = Image.open(BytesIO(base64.b64decode(image_data)))
-    image.save('uploaded_image.png')
-    socketio.emit('image_uploaded', {'url': '/show'})
-    return jsonify({'message': 'Image uploaded successfully'})
-
-# @app.route('/generate-url', methods=['GET'])
-# def generate_url():
-#     unique_url = f"{request.host_url}handwrite?user={uuid.uuid4()}"
-#     session['unique_url'] = unique_url  # 存儲到會話中
-#     return jsonify({'url': unique_url})
-
+# 產生唯一的網址 handwrite?session=
 @app.route('/generate-url', methods=['GET'])
 def generate_url():
     unique_url = f"{request.host_url}handwrite?session={uuid.uuid4()}"
     session['unique_url'] = unique_url  # 存儲到會話中
     return jsonify({'url': unique_url})
 
-
-@app.route('/get-url', methods=['GET'])
-def get_url():
-    unique_url = session.get('unique_url', None)  # 從會話中獲取 URL
-    if unique_url:
-        return jsonify({'url': unique_url})
-    else:
-        return jsonify({'error': 'URL not found'}), 404
+@app.route('/generate-url-qrcode')
+def generate_url_qrcode():
+    session_id = str(uuid.uuid4())  # 生成唯一的sessionID
+    unique_url = f"{request.host_url}comfirm_colordot?session={session_id}"
+    return jsonify(url=unique_url)
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
+
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -165,3 +190,4 @@ def handle_start_session(data):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    # socketio.run(app,host='0.0.0.0', port=5000, debug=True)
