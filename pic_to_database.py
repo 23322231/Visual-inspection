0e27a130105d81a736bc6e1ead268054fe938ce2
April-ying <132337602+April-ying@users.noreply.github.com>
2024-09-25 03:19:24 +0800

    資料庫加入圖片的檔案
    圖片檔名都改成答案

--- pic_to_database.py
+++ pic_to_database.py
@@ -9,17 +9,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:01057126@localhost/visual"
 db = SQLAlchemy(app)
 
-class ImageModel(db.Model):
-    __tablename__ = 'color_blind_question_pic'
-    id = Column(Integer, primary_key=True)
-    image_data = Column(LargeBinary, nullable=False)
-    answer = Column(Integer, nullable=False)
-
 # class ImageModel(db.Model):
-#     __tablename__ = 'color_blind_ans_pic'
-#     id_ans_cb = Column(Integer, primary_key=True)
+#     __tablename__ = 'color_blind_question_pic'
+#     id = Column(Integer, primary_key=True)
 #     image_data = Column(LargeBinary, nullable=False)
 #     answer = Column(Integer, nullable=False)
+
+class ImageModel(db.Model):
+    __tablename__ = 'color_blind_ans_pic'
+    id_ans_cb = Column(Integer, primary_key=True)
+    image_data = Column(LargeBinary, nullable=False)
+    answer = Column(Integer, nullable=False)
 
 
 def save_images_to_db(ids, image_paths, answers):
@@ -35,8 +35,8 @@ def save_images_to_db(ids, image_paths, answers):
             img_byte_arr = img_byte_arr.getvalue()
 
         # 創建一個新的ImageModel實例，並將其添加到列表中
-        # new_image = ImageModel(id_ans_cb=image_id, image_data=img_byte_arr, answer=answer)
-        new_image = ImageModel(id=image_id, image_data=img_byte_arr, answer=answer)
+        new_image = ImageModel(id_ans_cb=image_id, image_data=img_byte_arr, answer=answer)
+        # new_image = ImageModel(id=image_id, image_data=img_byte_arr, answer=answer)
         images.append(new_image)
     
     # 一次性將所有圖片批量插入到資料庫中
@@ -45,7 +45,7 @@ def save_images_to_db(ids, image_paths, answers):
     print(f"{len(images)} images and answers saved to database successfully.")
 
 if __name__ == "__main__":
-    id = [i for i in range(1, 33)]
+    id = [i for i in range(1, 31)]
     # image_paths = [
     #     "static/colorblind_image/1.png",
     #     "static/colorblind_image/2.png",
@@ -80,75 +80,71 @@ if __name__ == "__main__":
     #     "static/colorblind_image/37.png",
     #     "static/colorblind_image/38.png",
     # ]
-    # image_paths = [
-    #     "ans/1ans.png",
-    #     "ans/2ans.png",
-    #     "ans/3ans.png",
-    #     "ans/4ans.png",
-    #     "ans/5ans.png",
-    #     "ans/6ans.png",
-    #     "ans/7ans.png",
-    #     "ans/8ans.png",
-    #     "ans/9ans.png",
-    #     "ans/10ans.png",
-    #     "ans/11ans.png",
-    #     "ans/12ans.png",
-    #     "ans/13ans.png",
-    #     "ans/14ans.png",
-    #     "ans/15ans.png",
-    #     "ans/16ans.png",
-    #     "ans/17ans.png",
-    #     "ans/22ans.png",
-    #     "ans/23ans.png",
-    #     "ans/24ans.png",
-    #     "ans/25ans.png",
-    #     "ans/26ans.png",
-    #     "ans/27ans.png",
-    #     "ans/30ans.png",
-    #     "ans/31ans.png",
-    #     "ans/32ans.png",
-    #     "ans/33ans.png",
-    #     "ans/34ans.png",
-    #     "ans/35ans.png",
-    #     "ans/36ans.png",
-    #     "ans/37ans.png",
-    #     "ans/38ans.png",
-    # ]
     image_paths = [
-        "static/colorblind_image/1.jpg",
-        "static/colorblind_image/2.jpg",
-        "static/colorblind_image/3.jpg",
-        "static/colorblind_image/4.jpg",
-        "static/colorblind_image/5.jpg",
-        "static/colorblind_image/6.jpg",
-        "static/colorblind_image/7.jpg",
-        "static/colorblind_image/8.jpg",
-        "static/colorblind_image/9.jpg",
-        "static/colorblind_image/10.jpg",
-        "static/colorblind_image/11.jpg",
-        "static/colorblind_image/12.jpg",
-        "static/colorblind_image/13.jpg",
-        "static/colorblind_image/14.jpg",
-        "static/colorblind_image/15.jpg",
-        "static/colorblind_image/16.jpg",
-        "static/colorblind_image/17.jpg",
-        "static/colorblind_image/22.jpg",
-        "static/colorblind_image/23.jpg",
-        "static/colorblind_image/24.jpg",
-        "static/colorblind_image/25.jpg",
-        "static/colorblind_image/26.jpg",
-        "static/colorblind_image/27.jpg",
-        "static/colorblind_image/30.jpg",
-        "static/colorblind_image/31.jpg",
-        "static/colorblind_image/32.jpg",
-        "static/colorblind_image/33.jpg",
-        "static/colorblind_image/34.jpg",
-        "static/colorblind_image/35.jpg",
-        "static/colorblind_image/36.jpg",
-        "static/colorblind_image/37.jpg",
-        "static/colorblind_image/38.jpg",
+        "ans/2ans.jpg",
+        "ans/3ans.jpg",
+        "ans/5ans.jpg",
+        "ans/6ans.jpg",
+        "ans/7ans.jpg",
+        "ans/8ans.jpg",
+        "ans/12ans.jpg",
+        "ans/15ans.jpg",
+        "ans/16ans.jpg",
+        "ans/26ans.jpg",
+        "ans/29ans.jpg",
+        "ans/35ans.jpg",
+        "ans/42ans.jpg",
+        "ans/45ans.jpg",
+        "ans/57ans.jpg",
+        "ans/73ans.jpg",
+        "ans/74ans.jpg",
+        "ans/96ans.jpg",
+        "ans/97ans.jpg",
+        "ans/100ans.jpg",
+        "ans/101ans.jpg",
+        "ans/102ans.jpg",
+        "ans/103ans.jpg",
+        "ans/104ans.jpg",
+        "ans/105ans.jpg",
+        "ans/106ans.jpg",
+        "ans/107ans.jpg",
+        "ans/108ans.jpg",
+        "ans/109ans.jpg",
+        "ans/110ans.jpg",
     ]
-    answers = [12,8,6,29,57,5,3,15,74,2,6,97,45,5,7,16,73,26,42,35,96,100,100,100,100,100,100,100,100,100,100,100 ]  # 對應的答案列表 #不是數字的填100
+    # image_paths = [
+    #     "static/colorblind_image/2.jpg",
+    #     "static/colorblind_image/3.jpg",
+    #     "static/colorblind_image/5.jpg",
+    #     "static/colorblind_image/6.jpg",
+    #     "static/colorblind_image/7.jpg",
+    #     "static/colorblind_image/8.jpg",
+    #     "static/colorblind_image/12.jpg",
+    #     "static/colorblind_image/15.jpg",
+    #     "static/colorblind_image/16.jpg",
+    #     "static/colorblind_image/26.jpg",
+    #     "static/colorblind_image/29.jpg",
+    #     "static/colorblind_image/35.jpg",
+    #     "static/colorblind_image/42.jpg",
+    #     "static/colorblind_image/45.jpg",
+    #     "static/colorblind_image/57.jpg",
+    #     "static/colorblind_image/73.jpg",
+    #     "static/colorblind_image/74.jpg",
+    #     "static/colorblind_image/96.jpg",
+    #     "static/colorblind_image/97.jpg",
+    #     "static/colorblind_image/100.jpg",
+    #     "static/colorblind_image/101.jpg",
+    #     "static/colorblind_image/102.jpg",
+    #     "static/colorblind_image/103.jpg",
+    #     "static/colorblind_image/104.jpg",
+    #     "static/colorblind_image/105.jpg",
+    #     "static/colorblind_image/106.jpg",
+    #     "static/colorblind_image/107.jpg",
+    #     "static/colorblind_image/108.jpg",
+    #     "static/colorblind_image/109.jpg",
+    #     "static/colorblind_image/110.jpg",
+    # ]
+    answers = [2,3,5,6,7,8,12,15,16,26,29,35,42,45,57,73,74,96,97,100,101,102,103,104,105,106,107,108,109,110 ]  # 對應的答案列表 #不是數字的填100
 
     with app.app_context():
         db.create_all()  # 創建表
