57f998e56d90b2727caeacdc7ac5395efc15854a
April-ying <132337602+April-ying@users.noreply.github.com>
2024-10-20 00:33:18 +0800

    update:手部辨識

--- hand.py
+++ hand.py
@@ -1,107 +1,45 @@
 import cv2
 import mediapipe as mp
-import math
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# 根據兩點的座標，計算角度
-def vector_2d_angle(v1, v2):
-    v1_x = v1[0]
-    v1_y = v1[1]
-    v2_x = v2[0]
-    v2_y = v2[1]
-    try:
-        angle_= math.degrees(math.acos((v1_x*v2_x+v1_y*v2_y)/(((v1_x**2+v1_y**2)**0.5)*((v2_x**2+v2_y**2)**0.5))))
-    except:
-        angle_ = 180
-    return angle_
+# 啟動攝影機
+cap = cv2.VideoCapture(0)
 
-# 根據傳入的 21 個節點座標，得到該手指的角度
-def hand_angle(hand_):
-    angle_list = []
-    # thumb 大拇指角度
-    angle_ = vector_2d_angle(
-        ((int(hand_[0][0])- int(hand_[2][0])),(int(hand_[0][1])-int(hand_[2][1]))),
-        ((int(hand_[3][0])- int(hand_[4][0])),(int(hand_[3][1])- int(hand_[4][1])))
-        )
-    angle_list.append(angle_)
-    # index 食指角度
-    angle_ = vector_2d_angle(
-        ((int(hand_[0][0])-int(hand_[6][0])),(int(hand_[0][1])- int(hand_[6][1]))),
-        ((int(hand_[7][0])- int(hand_[8][0])),(int(hand_[7][1])- int(hand_[8][1])))
-        )
-    angle_list.append(angle_)
-    # middle 中指角度
-    angle_ = vector_2d_angle(
-        ((int(hand_[0][0])- int(hand_[10][0])),(int(hand_[0][1])- int(hand_[10][1]))),
-        ((int(hand_[11][0])- int(hand_[12][0])),(int(hand_[11][1])- int(hand_[12][1])))
-        )
-    angle_list.append(angle_)
-    # ring 無名指角度
-    angle_ = vector_2d_angle(
-        ((int(hand_[0][0])- int(hand_[14][0])),(int(hand_[0][1])- int(hand_[14][1]))),
-        ((int(hand_[15][0])- int(hand_[16][0])),(int(hand_[15][1])- int(hand_[16][1])))
-        )
-    angle_list.append(angle_)
-    # pink 小拇指角度
-    angle_ = vector_2d_angle(
-        ((int(hand_[0][0])- int(hand_[18][0])),(int(hand_[0][1])- int(hand_[18][1]))),
-        ((int(hand_[19][0])- int(hand_[20][0])),(int(hand_[19][1])- int(hand_[20][1])))
-        )
-    angle_list.append(angle_)
-    return angle_list
+def calculate_direction(finger_tip, finger_root):
+    """計算食指方向，回傳對應的方向名稱"""
+    # 計算向量 (指尖 - 指根)
+    direction_vector = np.array(finger_tip) - np.array(finger_root)
 
-# 根據手指角度的串列內容，返回對應的手勢名稱
-def hand_pos(finger_angle):
-    f1 = finger_angle[0]   # 大拇指角度
-    f2 = finger_angle[1]   # 食指角度
-    f3 = finger_angle[2]   # 中指角度
-    f4 = finger_angle[3]   # 無名指角度
-    f5 = finger_angle[4]   # 小拇指角度
+    # 定義四個基準方向向量
+    reference_vectors = {
+        "up": np.array([0, -1]),
+        "down": np.array([0, 1]),
+        "right": np.array([-1, 0]),
+        "left": np.array([1, 0])
+    }
 
-    # 小於 50 表示手指伸直，大於等於 50 表示手指捲縮
-    if f1<50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
-        return 'good'
-    elif f1>=50 and f2>=50 and f3<50 and f4>=50 and f5>=50:
-        return 'no!!!'
-    elif f1<50 and f2<50 and f3>=50 and f4>=50 and f5<50:
-        return 'ROCK!'
-    elif f1>=50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
-        return '0'
-    elif f1>=50 and f2>=50 and f3>=50 and f4>=50 and f5<50:
-        return 'pink'
-    elif f1>=50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
-        return '1'
-    elif f1>=50 and f2<50 and f3<50 and f4>=50 and f5>=50:
-        return '2'
-    elif f1>=50 and f2>=50 and f3<50 and f4<50 and f5<50:
-        return 'ok'
-    elif f1<50 and f2>=50 and f3<50 and f4<50 and f5<50:
-        return 'ok'
-    elif f1>=50 and f2<50 and f3<50 and f4<50 and f5>50:
-        return '3'
-    elif f1>=50 and f2<50 and f3<50 and f4<50 and f5<50:
-        return '4'
-    elif f1<50 and f2<50 and f3<50 and f4<50 and f5<50:
-        return '5'
-    elif f1<50 and f2>=50 and f3>=50 and f4>=50 and f5<50:
-        return '6'
-    elif f1<50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
-        return '7'
-    elif f1<50 and f2<50 and f3<50 and f4>=50 and f5>=50:
-        return '8'
-    elif f1<50 and f2<50 and f3<50 and f4<50 and f5>=50:
-        return '9'
-    else:
-        return ''
+    min_angle = 180  # 初始化最小角度
+    detected_direction = None
 
-cap = cv2.VideoCapture(0)            # 讀取攝影機
-fontFace = cv2.FONT_HERSHEY_SIMPLEX  # 印出文字的字型
-lineType = cv2.LINE_AA               # 印出文字的邊框
+    #計算與四個方向向量的夾角
+    for direction, ref_vector in reference_vectors.items():
+        #計算cos角度
+        cos_angle = np.dot(direction_vector[:2], ref_vector) / (
+            np.linalg.norm(direction_vector[:2]) * np.linalg.norm(ref_vector)
+        )
+        angle = np.degrees(np.arccos(cos_angle))  # 轉換為角度
 
-# mediapipe 啟用偵測手掌
+        if angle < min_angle:  # 找到最小夾角
+            min_angle = angle
+            detected_direction = direction
+
+    return detected_direction
+
+#偵測手掌
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
@@ -110,31 +48,48 @@ with mp_hands.Hands(
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
-    w, h = 540, 310                                  # 影像尺寸
+
     while True:
         ret, img = cap.read()
-        img = cv2.resize(img, (w,h))                 # 縮小尺寸，加快處理效率
         if not ret:
             print("Cannot receive frame")
             break
-        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 轉換成 RGB 色彩
-        results = hands.process(img2)                # 偵測手勢
+
+        # 影像處理：BGR 轉 RGB
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_rgb)
+
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                finger_points = []                   # 記錄手指節點座標的串列
-                for i in hand_landmarks.landmark:
-                    # 將 21 個節點換算成座標，記錄到 finger_points
-                    x = i.x*w
-                    y = i.y*h
-                    finger_points.append((x,y))
-                if finger_points:
-                    finger_angle = hand_angle(finger_points) # 計算手指角度，回傳長度為 5 的串列
-                    #print(finger_angle)                     # 印出角度 ( 有需要就開啟註解 )
-                    text = hand_pos(finger_angle)            # 取得手勢所回傳的內容
-                    cv2.putText(img, text, (30,120), fontFace, 5, (255,255,255), 10, lineType) # 印出文字
+                # 繪製手部骨架
+                # mp_drawing.draw_landmarks(
+                #     img, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                #     mp_drawing_styles.get_default_hand_landmarks_style(),
+                #     mp_drawing_styles.get_default_hand_connections_style()
+                # )
 
-        cv2.imshow('oxxostudio', img)
+                # 取得食指指尖 (8) 和指根 (5) 的座標
+                finger_tip = [
+                    hand_landmarks.landmark[8].x * img.shape[1],
+                    hand_landmarks.landmark[8].y * img.shape[0]
+                ]
+                finger_root = [
+                    hand_landmarks.landmark[5].x * img.shape[1],
+                    hand_landmarks.landmark[5].y * img.shape[0]
+                ]
+
+                # 計算指向方向
+                direction = calculate_direction(finger_tip, finger_root)
+                if direction:
+                    cv2.putText(
+                        img, f"Direction: {direction}", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA
+                    )
+
+        # 顯示影像
+        cv2.imshow('Hand Direction Detection', img)
         if cv2.waitKey(5) == ord('q'):
-            break
+            break  # 按下 q 鍵退出
+
 cap.release()
 cv2.destroyAllWindows()
