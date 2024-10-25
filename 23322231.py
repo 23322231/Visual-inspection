daf666af21ff292014e86ae3629c8822ae4d99f6
April <april910909@gmail.com>
2024-10-25 14:31:46 +0800

    給芷沂

---
+++ 23322231.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify, session
+from flask import Response,send_from_directory
+
+app = Flask(__name__)
+
+#點圖製作功能頁面
+@app.route('/')
+def elements():
+    return render_template('ishihara-test.html')
+
+#提供靜態檔案的路由，將內容類型指定為 JavaScript
+@app.route('/static/assets/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory('static/assets/js', filename, mimetype='text/javascript')
+
+
+if __name__ == '__main__':
+    # with app.app_context():
+    app.run(host='0.0.0.0', port=5000, debug=True)
